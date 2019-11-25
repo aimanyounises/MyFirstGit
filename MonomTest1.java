f23eb22a8e3858529c9cf13e8e3fb3e29343c46e
aimanyounises <aimanyounises@gmail.com>
2019-11-25 18:08:42 +0200

    new MonomTest1.java

---
+++ MonomTest1.java
@@ -0,0 +1,14 @@
+package myMath;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MonomTest1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
